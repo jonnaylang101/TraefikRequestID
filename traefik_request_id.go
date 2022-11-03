99d82b67bdf6ecc6613ec7713aec3dd67517598d
John Hughes <john.hughes@musictribe.com>
2022-11-03 11:11:28 +0000

    build: :wrench: Add traefik boilerplate

--- traefik_request_id.go
+++ traefik_request_id.go
@@ -1,6 +1,7 @@
 package traefikrequestid
 
 import (
+	"context"
 	"errors"
 	"net/http"
 
@@ -8,6 +9,29 @@ import (
 )
 
 const reqIdHeaderKey string = "X-Request-ID"
+
+type Config struct{}
+
+func CreateConfig() *Config {
+	return &Config{}
+}
+
+type RequestID struct {
+	next http.Handler
+	name string
+}
+
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	return &RequestID{
+		next: next,
+		name: name,
+	}, nil
+}
+
+func (rid *RequestID) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	rw.Write([]byte("hello world!\n"))
+	// rid.next.ServeHTTP(rw, req)
+}
 
 func addRequestIdToHeaders(httpRequest *http.Request, requestId uuid.UUID) error {
 	if httpRequest == nil {
